bf42fac7a00bf340514e7474956ab66a09f6fa88
Luiz Souza <depositodivisa@gmail.com>
2026-06-12 13:31:08 -0300

    20260612ok

--- src/main/java/org/hopto/demo/util/mainApp.java
+++ src/main/java/org/hopto/demo/util/mainApp.java
@@ -1,24 +1,31 @@
 package org.hopto.demo.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class mainApp implements ApplicationRunner {
-    @Autowired
-    private CasoJudicial casoJudicial;
-    
-    public static void main(String[] args) {
-        SpringApplication.run(mainApp.class, args);
-        
+public class MainApp implements ApplicationRunner {
+    private final  CasoJudicial casoJudicial;
 
+   MainApp(CasoJudicial casoJudicial) {
+      this.casoJudicial = casoJudicial;
+   }
+public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
     }
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        // startup logic can be placed here
         System.out.println("Aplicação iniciada com sucesso!");
+        double custoFinal = casoJudicial.finalizarCusto(5000.0, "SP", 2005);
+        System.out.println("Custo final do caso judicial: " + custoFinal);
     }
-    }
+}
+
+
+
+// This component will run after the application starts
