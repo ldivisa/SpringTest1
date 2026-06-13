aca7f371a9f7c1afecbdfc44c5eb035537ad30db
= <=>
2026-06-13 19:56:09 -0300

    31062026-11

--- src/main/java/org/hopto/demo/util/CasoJudicialController.java
+++ src/main/java/org/hopto/demo/util/CasoJudicialController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -39,5 +40,33 @@ public class CasoJudicialController {
     public ResponseEntity<CasoJudicial> criarCaso(@RequestBody CasoJudicial caso) {
         CasoJudicial novoCaso = service.criarCaso(caso);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoCaso);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<CasoJudicial> atualizarCaso(@PathVariable Long id, @RequestBody CasoJudicial caso) {
+        CasoJudicial casoAtualizado = service.atualizarCaso(id, caso);
+        if (casoAtualizado != null) {
+            return ResponseEntity.ok(casoAtualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }   
+    @PatchMapping("/{id}")
+    public ResponseEntity<CasoJudicial> atualizarParcialCaso(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
+        CasoJudicial casoAtualizado = service.atualizarParcialCaso(id, updates);
+        if (casoAtualizado != null) {
+            return ResponseEntity.ok(casoAtualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }  
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCaso(@PathVariable Long id) {
+        boolean deletado = service.deletarCaso(id);
+        if (deletado) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
