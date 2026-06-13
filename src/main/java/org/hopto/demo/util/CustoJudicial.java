e2e7172a3ae0f92e05e32b35b6e64c9929c5e27c
Luiz Souza <depositodivisa@gmail.com>
2026-06-13 11:24:32 -0300

    13062026-1

--- src/main/java/org/hopto/demo/util/CustoJudicial.java
+++ src/main/java/org/hopto/demo/util/CustoJudicial.java
@@ -5,23 +5,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustoJudicial {
 
-    
-    
     public CustoJudicial(){
         }
-
-    
-
-
 
     public double adicionarCusto(int ano)
     {
         if (ano < 2000){
             return 1000.0;
         } else if (ano >= 2000 && ano < 2010){
-            return 2000.0;
+            return 5000.0;
         } else {
-            return 3000.0;
+            return 30000.0;
         }
     }
 
--- src/main/java/org/hopto/demo/util/mainApp.java
+++ src/main/java/org/hopto/demo/util/mainApp.java
@@ -1,28 +1,27 @@
 package org.hopto.demo.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class mainApp implements ApplicationRunner {
-    private final  CasoJudicial casoJudicial;
+public class mainApp implements  CommandLineRunner{
+ @Autowired
+    private CasoJudicial casoJudicial;
 
-   mainApp(CasoJudicial casoJudicial) {
-      this.casoJudicial = casoJudicial;
-   }
 public static void main(String[] args) {
         SpringApplication.run(mainApp.class, args);
     }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Aplicação iniciada com sucesso!");
-        double custoFinal = casoJudicial.finalizarCusto(5000.0, "SP", 2005);
+@Override
+    public void run(String[] args) throws Exception {
+        double custoFinal = casoJudicial.finalizarCusto(10000, "SP",2005);
         System.out.println("Custo final do caso judicial: " + custoFinal);
     }
 }
+
 
 
 
